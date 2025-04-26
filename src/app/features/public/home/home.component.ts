e6ca04ca3b5bcaa8ffce1b96192bf1fd8f6342ed
RajeshChowdaryP <paidirajesh2001@gmail.com>
2025-04-26 21:56:13 +0530

    Home page enhacements and added blog detail component

--- src/app/features/public/home/home.component.ts
+++ src/app/features/public/home/home.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { BlogpostService } from '../../services/blogpost.service';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { BlogPost } from 'src/app/model/blogPost';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -11,11 +12,26 @@ import { BlogPost } from 'src/app/model/blogPost';
 })
 export class HomeComponent implements OnInit {
   blogs$?: Observable<BlogPost[]> = undefined;
+
+  searchText: FormControl = new FormControl('');
+  blogPosts: BlogPost[] = [];
+
   constructor(private blogPostService: BlogpostService) { }
 
+  
   ngOnInit(): void {
     // Initialize component
     this.blogs$ = this.blogPostService.getAllBlogPosts();
+
+    this.blogPostService.getAllBlogPosts().subscribe((data) => {
+      this.blogPosts = data;
+    });
+
+    this.searchText.valueChanges.subscribe((value) => {
+      const filteredPosts = this.blogPosts.filter((blogPost) => { return blogPost.title.toLowerCase().includes(value.toLowerCase());
+      });
+      this.blogs$ = of(filteredPosts);
+    });
   }
 
 }
